518ef0c607812d1227f5217fb009ca90c528c1aa
Rogerio Black <rogeriodevelop@gmail.com>
2019-03-19 20:32:38 -0300

    Funções de sessão

--- exemplo-04.php
+++ exemplo-04.php
@@ -1,7 +1,11 @@
 <?php
 
+session_id('0ld6su3u3q175coerbuehh5qp2');
+
 require_once("config.php");
 
 session_regenerate_id();
 
 echo session_id();
+
+var_dump($_SESSION);
---
+++ exemplo-05.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once("config.php");
+
+echo session_save_path();
+
+echo "<br/>";
+
+var_dump(session_status());
+
+
+switch (session_status()) {
+	case PHP_SESSION_DISABLED:
+	echo "as sessões estão desabilitadas";
+	break;
+
+	case PHP_SESSION_NONE:
+	echo "as sessões estão habilitadas mas não iniciadas";
+	break;
+
+	case PHP_SESSION_ACTIVE:
+	echo "as sessões estão Habilitadas e uma sessão existe";
+	break;
+
+}
